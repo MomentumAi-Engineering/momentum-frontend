9cf52f2f2db956bb47ac8617a5d25e32402d700e
Raj <rajbaidyanath76@gmail.com>
2025-06-19 02:08:03 +0530

    db auth done

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -26,7 +26,7 @@ export default function Signup() {
 
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("http://localhost:5000/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
